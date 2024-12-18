85b2312f08a2d772dc9971211bfb812decc9e60a
antoneosphilip <109253442+antoneosphilip@users.noreply.github.com>
2024-12-18 05:28:57 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,96 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:rommify_app/core/routing/app_router.dart';
+import 'package:rommify_app/main_rommify.dart';
 
-void main() {
-  runApp(MyApp());
+import 'core/helpers/shared_pref_helper.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  await SharedPrefHelper.init();
+  runApp( RoomifyApp(appRouter: AppRouter(),));
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SunEffectScreen(),
-    );
-  }
-}
 
-class SunEffectScreen extends StatefulWidget {
-  @override
-  _SunEffectScreenState createState() => _SunEffectScreenState();
-}
-
-class _SunEffectScreenState extends State<SunEffectScreen> with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-  late Animation<Color?> _colorAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Initialize the AnimationController for smooth animation
-    _controller = AnimationController(
-      duration: Duration(seconds: 5),
-      vsync: this,
-    )..repeat(reverse: true);
-
-    // Animation for position (for the sun to move horizontally)
-    _animation = Tween<double>(begin: 0, end: 1).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.easeInOut,
-    ));
-
-    // Animation for the color change
-    _colorAnimation = ColorTween(begin: Colors.orange, end: Colors.yellow).animate(
-      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
-    );
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return CustomPaint(
-              size: Size(double.infinity, double.infinity),
-              painter: SunPainter(_animation.value, _colorAnimation.value),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class SunPainter extends CustomPainter {
-  final double position;
-  final Color? color;
-
-  SunPainter(this.position, this.color);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final Paint paint = Paint()
-      ..color = color!
-      ..style = PaintingStyle.fill;
-
-    // Drawing the sun as a circle
-    double x = position * size.width; // Moves horizontally
-    double y = size.height * 0.2; // Keeps sun at a fixed vertical position
-
-    canvas.drawCircle(Offset(x, y), 50, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
-  }
-}
